37bd455fcb2df424d9f469031be6d2a049a7526e
Marcel <mvelasquez2106@gmail.com>
2025-12-11 18:58:53 +0100

    Baic Window

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,24 @@
-fn main() 
+use macroquad::prelude::*;
+
+fn window_config() -> Conf
 {
-    println!("Hello, world!");
+    Conf
+    {
+        window_title: "Maze Image".to_owned(),
+        window_width: 1280,
+        window_height: 720,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_config)]
+async fn main() 
+{
+    loop 
+    {
+        clear_background(Color::new(0.164705882, 0.164705882, 0.164705882, 1.0));
+        
+
+        next_frame().await
+    }
 }
