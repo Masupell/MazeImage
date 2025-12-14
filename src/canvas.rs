7b5c87adb59e3a58ee200f8d39d168f642c54065
Marcel <mvelasquez2106@gmail.com>
2025-12-14 22:36:18 +0100

    Brush Smoothing

--- src/canvas.rs
+++ src/canvas.rs
@@ -6,6 +6,8 @@ pub struct Canvas
     pub texture: Texture2D,
     last_pos: Option<Vec2>,
     brush_radius: f32,
+    smooth_pos: Vec2,
+    smoothing_factor: f32,
 }
 
 impl Canvas 
@@ -21,6 +23,8 @@ impl Canvas
             texture,
             last_pos: None,
             brush_radius: 6.0,
+            smooth_pos: vec2(0.0, 0.0),
+            smoothing_factor: 0.5
         }
     }
 
@@ -30,13 +34,19 @@ impl Canvas
 
         if is_mouse_button_down(MouseButton::Left) 
         {
-            if let Some(last) = self.last_pos 
+            if self.last_pos.is_none()
             {
-                self.draw_line(last, mouse);
+                self.smooth_pos = mouse;
             }
-            self.last_pos = Some(mouse);
-        } 
-        else 
+            self.smooth_pos = self.smooth_pos.lerp(mouse, self.smoothing_factor);
+
+            if let Some(last) = self.last_pos
+            {
+                self.draw_line(last, self.smooth_pos);
+            }
+            self.last_pos = Some(self.smooth_pos);
+        }
+        else
         {
             self.last_pos = None;
         }
@@ -44,12 +54,12 @@ impl Canvas
         self.texture.update(&self.canvas);
     }
 
-    fn draw_line(&mut self, a: Vec2, b: Vec2) 
+    fn draw_line(&mut self, a: Vec2, b: Vec2)
     {
         let dist = a.distance(b);
         let steps = dist.max(1.0) as i32;
 
-        for i in 0..=steps 
+        for i in 0..=steps
         {
             let t = i as f32 / steps as f32;
             let p = a.lerp(b, t);
@@ -57,15 +67,15 @@ impl Canvas
         }
     }
 
-    fn draw_brush(&mut self, pos: Vec2) 
+    fn draw_brush(&mut self, pos: Vec2)
     {
         let r = self.brush_radius as i32;
 
-        for y in -r..=r 
+        for y in -r..=r
         {
-            for x in -r..=r 
+            for x in -r..=r
             {
-                if x * x + y * y <= r * r 
+                if x * x + y * y <= r * r
                 {
                     let px = pos.x as i32 + x;
                     let py = pos.y as i32 + y;
